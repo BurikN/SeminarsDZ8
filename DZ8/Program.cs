47662db41283b02a4d3cc9b98e35da9a05cfe7cc
Nikita <tesh-123@mail.ru>
2022-10-12 18:07:53 +0300

    Добавил 1 и 2 домашнее задание по 8 семинару

---
+++ DZ8/Program.cs
@@ -0,0 +1,112 @@
+﻿// Задача 1
+/*
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int sumRow = 0;
+int maxLine = 0;
+int max = sumRow;
+int temp = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp += matrix[i, j];
+        sumRow = temp;
+    }
+    temp = 0;
+
+    if (max < sumRow)
+    {
+        max = sumRow;
+        maxLine = i + 1;
+    }
+    Console.WriteLine($"Сумма {i + 1} строки = {sumRow} ");
+}
+Console.WriteLine($"Максимальная строка {maxLine} её сумма = {max} ");
+*/
+//Задача 2
+/*
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstMatrix = FillMatrix(m, n);
+int[,] secondMatrix = FillMatrix(m, n);
+
+Console.WriteLine($"Первая матрица ");
+PrintMatrix(firstMatrix);
+Console.WriteLine($"Вторая матрица ");
+PrintMatrix(secondMatrix);
+Console.WriteLine($"Произведение индексов первой и второй матрицы ");
+
+int[,] multiMatrix = multiMatrix = new int[m, n];
+for (int i = 0; i < multiMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < multiMatrix.GetLength(1); j++)
+    {
+        multiMatrix[i, j] = firstMatrix[i, j] * secondMatrix[i, j];
+    };
+}
+
+PrintMatrix(multiMatrix);
+*/
+//Задача 3
